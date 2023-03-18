d56084709017ea00616957491f2f4dd0574c2d92
Carter Croft <80287993+cartercroft@users.noreply.github.com>
2023-03-18 10:56:58 -0500

    Updates

---
+++ Classes/ToastService.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Options;
+using System.Timers;
+
+namespace CroftBlazorComponents.Services
+{
+    public class ToastService : IDisposable
+    {
+        public event Action<string, ToastLevel>? OnShow;
+        public event Action? OnHide;
+        private int Interval;
+        private System.Timers.Timer? Countdown;
+        const int __DEFAULTINTERVAL__ = 5000;
+        public ToastService()
+        {
+            Interval = __DEFAULTINTERVAL__;
+        }
+        public ToastService(IOptions<ToastServiceOptions> options)
+        {
+            Interval = options.Value.Interval ?? __DEFAULTINTERVAL__;
+        }
+
+        public void ShowToast(string message, ToastLevel level)
+        {
+            OnShow?.Invoke(message, level);
+            StartCountdown();
+        }
+        public void ShowToast(string message, ToastLevel level, int millis)
+        {
+            OnShow?.Invoke(message, level);
+            StartCountdown(millis);
+        }
+
+        private void StartCountdown()
+        {
+            SetCountdown();
+
+            if (Countdown!.Enabled)
+            {
+                Countdown.Stop();
+                Countdown.Start();
+            }
+            else
+            {
+                Countdown!.Start();
+            }
+        }
+        private void StartCountdown(int millis)
+        {
+            SetCountdown(millis);
+
+            if (Countdown!.Enabled)
+            {
+                Countdown.Stop();
+                Countdown.Start();
+            }
+            else
+            {
+                Countdown!.Start();
+            }
+        }
+
+        private void SetCountdown()
+        {
+            Countdown = new System.Timers.Timer(Interval);
+            Countdown.Elapsed += HideToast;
+            Countdown.AutoReset = false;
+        }
+
+        private void SetCountdown(int millis)
+        {
+            Countdown = new System.Timers.Timer(millis);
+            Countdown.Elapsed += HideToast;
+            Countdown.AutoReset = false;
+        }
+        private void HideToast(object? source, ElapsedEventArgs args)
+        {
+            OnHide?.Invoke();
+        }
+
+
+        public void Dispose()
+            => Countdown?.Dispose();
+    }
+    public enum ToastLevel
+    {
+        Info,
+        Warning,
+        Success,
+        Error
+    }
+
+    public class ToastServiceOptions 
+    {
+        public int? Interval { get; set; }
+    }
+}
---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CroftBlazorComponents.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroftBlazorComponents.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddComponentServices(this IServiceCollection services)
+        {
+            services.AddTransient<ToastService>();
+        }
+    }
+}
